6cee008c6c39fde85ec5cd705c05ac6423331131
Bruno Bartolomeu <brunoowap@gmail.com>
2022-05-10 17:59:22 -0300

    Corrigindo button

--- src/components/formularioComponent/FormularioComponent.jsx
+++ src/components/formularioComponent/FormularioComponent.jsx
@@ -1,8 +1,25 @@
-import InputComponent from "../input-component/InputComponent"
-function Formulario(){
-  return (
-    <InputComponent placeholder="Enter your name..."/>
-  );
+import InputComponent from "../input-component/InputComponent";
+function Formulario() {
+	function teste(){
+
+		const algo = document.getElementsByTagName("InputComponent").value;
+		console.log(algo);
+	/* 	if (props.title.length > 0) {
+			alert("DADOS ENVIADOS");
+		} else alert("Existe campos incompletos");
+		 */
+	}
+	
+	return (
+		<div>
+			<InputComponent placeholder="Enter your name..." title="NAME" />
+			<InputComponent
+				placeholder="Your email address..."
+				title="EMAIL ADDRESS"
+			/>
+			<button onClick={teste}>Submite</button>
+		</div>
+	);
 }
 
 export default Formulario;
--- src/components/input-component/InputComponent.jsx
+++ src/components/input-component/InputComponent.jsx
@@ -3,19 +3,19 @@ function InputComponent(props){
 	
 
 	function teste(){
-		const input = document.querySelector(".inputContainer").value;
+/* 		const input = document.querySelector(".inputContainer").value;
 		if(input.length > 0){
 			alert("DADOS ENVIADOS");
 		}
 		else alert("Existe campos incompletos")
-		console.log(input);
+		console.log(input); */
 	}
 	return(
 		<div>
-			<h1>Input</h1>
+			<h6>{props.title}</h6>
 			<input className="inputContainer" id='teste' type="text" placeholder={props.placeholder}/>
-			<button onClick={teste}>Submite</button>
-		</div>
+{/* 			<button onClick={teste}>Submite</button>
+ */}		</div>
 	);
 }
 
