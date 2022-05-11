68a58ddf9efd1925f480fce72ed1e3d9e578225e
Pedro Machado <pedro.machado@valtech.com>
2022-05-11 10:35:51 -0300

    footer initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 
 function App() {
 
-  return (<h1>Hello World</h1>)
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Footer/>
+    </>
+  
+  )
 }
 
 export default App
