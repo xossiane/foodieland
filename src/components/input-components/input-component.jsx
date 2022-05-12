852b291c85a762ca82dcf57ebfbde1f34e6f9e7d
xossiane <anejosi.lima@gmail.com>
2022-05-12 16:57:34 -0300

    finalInputComponent2

--- src/components/input-components/input-component.jsx
+++ src/components/input-components/input-component.jsx
@@ -1,7 +1,7 @@
 import './input-component.css'
 import '../outsideContainer/outsideContainer.css'
-import '../outsideContainer/carregarImagem'
-
+import direita from '../images/direita.png'
+import esquerda from '../images/esquerda.png'
 
     function InputComponent (props){
         
@@ -9,24 +9,27 @@ import '../outsideContainer/carregarImagem'
         function teste(){ 
             const input = document.querySelector(".inputContainer").value;
             if (input.length > 0){
-                alert("Dados Enviados");
+                alert("Thanks for your subscription");
             }
         
-            else alert("Existem campos não preenchidos")
+            else alert("Please, enter your email adress for more recipes")
             console.log(input); 
         }
             return (
                 <div className="outsideContainer">
-                    <carregarImagem />
+                    <img className= 'direitaImage' src={direita} />
+                    <img className= 'esquerdaImage' src={esquerda} />
                     <div className="titleContainer">
                     <h1>Deliciousness to your inbox</h1>
                     </div>
                     <div className="textContainer">
                     <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
                     </div>
+                
                     <input className= "inputContainer" type="text" placeholder={props.texto} />
                     <div className="containerButton">
                     <button className="submit_button" onClick = {teste}>Submit</button>
+                    
                     </div>
                 </div>
         );
