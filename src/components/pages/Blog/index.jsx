390d60b5c411ca70592694c1d09289f49820faae
KenjiWorkES <lucas.kenjies@gmail.com>
2022-05-27 13:38:00 -0300

    Setando margin-right: 0 na pseudo-classe navigation__item:last-child

--- src/components/pages/Blog/index.jsx
+++ src/components/pages/Blog/index.jsx
@@ -1,12 +1,8 @@
 import { Navbar, Footer, NewsletterForm } from "../../organisms";
-import { NavigationItem } from "../../atoms";
 export default function Blog() {
   return (
     <>
       <Navbar />
-      <NavigationItem label="1" />
-      <NavigationItem label="2" />
-      <NavigationItem label="3" className="navigation__item--active" />
       <NewsletterForm />
       <Footer />
     </>
