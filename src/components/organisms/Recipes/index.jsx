67128077b845a766a095a25b290c5997a29f1811
Bruno Bartolomeu <brunoowap@gmail.com>
2022-05-30 16:17:53 -0300

    Finalizando a responsividade do componente Recipes

--- src/components/organisms/Recipes/index.jsx
+++ src/components/organisms/Recipes/index.jsx
@@ -4,15 +4,11 @@ import Date from "../../../data/recipes.json";
 import "./Recipes.scss";
 function Recipes() {
   return (
-    <div>
-      <div className="recipes-container">
-        <Advertising className="recipes-container__img" />
-        <h1 className="recipes-container__title">Tasty Recipes</h1>
-        <div className="recipes-container__receitas">
-          <div>
-            <Recipe dados={Date} />
-          </div>
-        </div>
+    <div className="recipes-container">
+      <Advertising className="recipes-container__img" />
+      <h1 className="recipes-container__title">Tasty Recipes</h1>
+      <div className="recipes-container__receitas">
+        <Recipe dados={Date} />
       </div>
     </div>
   );
