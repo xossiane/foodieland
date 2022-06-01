3a3eec88452eae5cca2728a6fa9d880d88d9ea06
KenjiWorkES <lucas.kenjies@gmail.com>
2022-06-01 14:30:20 -0300

    migrando css de cardTittle, cardInfo e carImage para scss

--- src/components/atoms/CardInfo/index.jsx
+++ src/components/atoms/CardInfo/index.jsx
@@ -8,11 +8,11 @@ const CardInfo = (props) => {
     <div className="card__info">
       <div className="card__info__item">
         <img src={ForkKnife} />
-        <p>{props.firstInfo}</p>
+        <p className="card__info__text">{props.firstInfo}</p>
       </div>
       <div className="card__info__item">
         <img src={Timer} />
-        <p>{props.secondInfo}</p>
+        <p className="card__info__text">{props.secondInfo}</p>
       </div>
     </div>
   );
