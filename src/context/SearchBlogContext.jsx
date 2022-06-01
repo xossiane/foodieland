c29da61f854997675458276dafd548c3efb19f15
KenjiWorkES <lucas.kenjies@gmail.com>
2022-06-01 12:19:48 -0300

    Navegação Desktop feita

--- src/context/SearchBlogContext.jsx
+++ src/context/SearchBlogContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import articles from "/src/data/articles.json";
 
 export const SearchBlogContext = React.createContext({
@@ -9,13 +9,74 @@ export const SearchBlogContext = React.createContext({
   setCurrentPage: () => {},
   currentPage: "",
   navigationItems: "",
+  windowSize: 0,
 });
+
+const paginationReducer = (state, action) => {
+  if (action.type === "CHANGE__CURRENT__PAGE") {
+    return {
+      itensPerPage: state.itensPerPage,
+      currentPage: action.value,
+      navigationNumber: state.navigationNumber,
+      hasPageNumber: state.hasPageNumber,
+    };
+  }
+  if (action.type === "CHANGE_RESOLUTION") {
+    return {
+      itensPerPage: action.itensPerPage,
+      currentPage: state.currentPage,
+      navigationNumber: action.navigationNumber,
+      hasPageNumber: action.hasPageNumber,
+    };
+  }
+};
 
 const SearchBlogProvider = ({ children }) => {
   const [searchInput, setSearchInput] = useState("");
+  const [windowSize, setWindowSize] = useState(window.innerWidth);
 
-  const [itensPerPage, setItensPerPage] = useState(3);
-  const [currentPage, setCurrentPage] = useState(0);
+  /*const [itensPerPage, setItensPerPage] = useState(3);
+  const [currentPage, setCurrentPage] = useState(0);*/
+
+  const paginationDefaultValue = {
+    itensPerPage: 3,
+    currentPage: 0,
+    navigationNumber: 3,
+    hasPageNumber: 2,
+  };
+
+  const [paginationState, dispatchPaginationState] = useReducer(
+    paginationReducer,
+    paginationDefaultValue
+  );
+
+  useEffect(() => {
+    let timeoutId = null;
+    const updateSize = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setWindowSize(window.innerWidth), 150);
+    };
+    window.addEventListener("resize", updateSize);
+  }, []);
+  console.log(windowSize);
+
+  useEffect(() => {
+    if (windowSize > 1024) {
+      dispatchPaginationState({
+        type: "CHANGE_RESOLUTION",
+        itensPerPage: 6,
+        navigationNumber: 5,
+        hasPageNumber: 4,
+      });
+    } else {
+      dispatchPaginationState({
+        type: "CHANGE_RESOLUTION",
+        itensPerPage: 3,
+        navigationNumber: 3,
+        hasPageNumber: 2,
+      });
+    }
+  }, [windowSize]);
 
   const getFilteredArray = () => {
     let array = articles.filter((item) =>
@@ -25,22 +86,29 @@ const SearchBlogProvider = ({ children }) => {
   };
 
   const changeCurrentPageHandler = (index) => {
-    setCurrentPage(index);
+    dispatchPaginationState({ type: "CHANGE__CURRENT__PAGE", value: index });
   };
 
   const searchInputHandler = (e) => {
     setSearchInput(e.target.value);
   };
 
-  const pages = Math.ceil(getFilteredArray().length / itensPerPage);
-  const startIndex = currentPage * itensPerPage;
-  const endIndex = itensPerPage + startIndex;
+  const pages = Math.ceil(
+    getFilteredArray().length / paginationState.itensPerPage
+  );
+  const startIndex = paginationState.currentPage * paginationState.itensPerPage;
+  const endIndex = paginationState.itensPerPage + startIndex;
+  const lastPage = pages - 1;
   let navigationItems = [];
 
-  if (currentPage + 2 < pages - 1) {
-    navigationItems = [currentPage, currentPage + 1, currentPage + 2];
+  if (paginationState.currentPage + paginationState.hasPageNumber < lastPage) {
+    for (let i = 0; i < paginationState.navigationNumber; i++) {
+      navigationItems.push(paginationState.currentPage + i);
+    }
   } else {
-    navigationItems = [pages - 3, pages - 2, pages - 1];
+    for (let i = paginationState.navigationNumber; i > 0; i--) {
+      navigationItems.push(pages - i);
+    }
   }
 
   const dataArticles = getFilteredArray().slice(startIndex, endIndex);
@@ -54,8 +122,9 @@ const SearchBlogProvider = ({ children }) => {
         pages: pages,
         dataArticles: dataArticles,
         setCurrentPage: changeCurrentPageHandler,
-        currentPage: currentPage,
+        currentPage: paginationState.currentPage,
         navigationItems: navigationItems,
+        windowSize: windowSize,
       }}
     >
       {children}
