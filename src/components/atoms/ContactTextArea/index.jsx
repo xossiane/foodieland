f409b418563c96e230ccf8ad9937bc22b7c70db9
Bruno Bartolomeu <brunoowap@gmail.com>
2022-06-02 11:54:23 -0300

    Corrigindo scss do componente TextArea

--- src/components/atoms/ContactTextArea/index.jsx
+++ src/components/atoms/ContactTextArea/index.jsx
@@ -3,10 +3,10 @@ import "./TextAreaComponent.scss";
 
 function selectComponent(props) {
   return (
-    <div className="textArea__container">
+    <div className="textArea-container">
       <h6 className="input-container__label">{props.title}</h6>
       <textarea
-        className="textArea"
+        className="textArea-container__textArea"
         placeholder={props.placeholder}
         value={props.value}
         onChange={props.onTextChange}
