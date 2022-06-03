cf6b4c2294d869043179334c3f290844c4a5fecc
KenjiWorkES <lucas.kenjies@gmail.com>
2022-06-03 09:54:53 -0300

    Tirando dependencias que não estão mais sendo usadas no context

--- src/context/SearchBlogContext.jsx
+++ src/context/SearchBlogContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useContext } from "react";
+import React, { useState } from "react";
 import articles from "/src/data/articles.json";
 import usePagination from "../hooks/usePagination";
 
