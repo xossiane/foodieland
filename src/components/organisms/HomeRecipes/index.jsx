29001fa72fb54ef8723c46679b230e0d98004594
xossiane <anejosi.lima@gmail.com>
2022-06-13 21:18:31 -0300

    Criado JSON para as receitas e aplicação das imagens

--- src/components/organisms/HomeRecipes/index.jsx
+++ src/components/organisms/HomeRecipes/index.jsx
@@ -1,24 +1,23 @@
 import { CardImage, CardTitle, LikeButton } from "../../atoms";
 import { useContext } from "react";
-import { BlogContext } from "/src/context/BlogContext";
-
+import homerecipes from "/src/data/homerecipes.json";
 
 function HomeRecipes (props) {
-    const { dataArticles } = useContext(BlogContext);
+    
   //const article = getFilteredArray();
-    const article = dataArticles;
+    const recipes = homerecipes;
 return (
     <div className="Recipes__post">
         <h1 className="HomeRecipes__title">Simple and tasty recipes</h1>
         <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
-        {article.map((article) => {
+        {recipes.map((recipes) => {
             return (
                 <>
-                <article key={article.id} className="HomeRecipes__post__item">
-                    <CardTitle title={article.subtitle} />
+                <article key={recipes.id} className="HomeRecipes__post__item">
+                    <CardTitle title={recipes.title} />
                     <CardImage
                         className="HomeRecipes__image--mt"
-                        image={article.img}></CardImage>
+                        image={recipes.img}></CardImage>
                     <LikeButton></LikeButton>
                 </article></>
                 
