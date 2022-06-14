72f72196f813343594f4626fe8a9643d12381d84
KenjiWorkES <lucas.kenjies@gmail.com>
2022-06-14 15:16:33 -0300

    criando json de categorias e seus modificadores

--- src/components/molecules/CategoriesCard/index.jsx
+++ src/components/molecules/CategoriesCard/index.jsx
@@ -1,10 +1,10 @@
 import "./CategoriesCard.scss";
 
 const CategoriesCard = (props) => {
-  const classes = "categories__card " + props.className;
+  const classes = props.className ? props.className : "";
 
   return (
-    <div className={classes}>
+    <div className={"categories__card " + classes}>
       <img className="categories__image" src={props.image} alt="" />
       <p className="categories__text">{props.text}</p>
     </div>
