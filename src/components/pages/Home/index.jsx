78edd29aac8ccc2880a3a9ca75af8a65e3287ca4
Bruno Bartolomeu <brunoowap@gmail.com>
2022-06-14 10:58:05 -0300

    Criando JSX do homeHeader e o JSON para o card no homeHeader

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -1,9 +1,9 @@
-import { Navbar, Footer } from "../../organisms";
+import { Navbar, Footer, HomeHeader } from "../../organisms";
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1>Home</h1>
+      <HomeHeader />
       <Footer />
     </div>
   );
