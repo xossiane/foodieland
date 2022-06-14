1ff61e6a0f6d5e9b7d4f464b16f73270588e0129
Bruno Bartolomeu <brunoowap@gmail.com>
2022-06-14 11:18:09 -0300

    Alterando o Json do homeHeader para ficar mais completo

--- src/components/organisms/HomeHeader/index.jsx
+++ src/components/organisms/HomeHeader/index.jsx
@@ -1,7 +1,18 @@
+import data from "../../../data/homeHeader.json";
 export default function HomeHeader() {
   return (
     <div>
-      <h1>Card</h1>
+      <div>
+        {data.map((dado) => (
+          <div key={dado.id}>
+            <img src={dado.img} />
+            <div>
+              <h2>{dado.title}</h2>
+              <h3>{dado.author}</h3>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
