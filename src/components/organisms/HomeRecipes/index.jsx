c62b508bbb72e73cce19e919f5c71ffd071ae425
xossiane <anejosi.lima@gmail.com>
2022-06-14 10:38:04 -0300

    Importando o componente Card no Slider do HomeRecipes

--- src/components/organisms/HomeRecipes/index.jsx
+++ src/components/organisms/HomeRecipes/index.jsx
@@ -1,28 +1,24 @@
-import { CardImage, CardTitle, LikeButton } from "../../atoms";
-import { useContext } from "react";
+import { Card } from "../../molecules";
 import homerecipes from "/src/data/homerecipes.json";
 
 function HomeRecipes (props) {
     
-  //const article = getFilteredArray();
     const recipes = homerecipes;
 return (
     <div className="Recipes__post">
         <h1 className="HomeRecipes__title">Simple and tasty recipes</h1>
         <p>Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
-        {recipes.map((recipes) => {
-            return (
-                <>
-                <article key={recipes.id} className="HomeRecipes__post__item">
-                    <CardTitle title={recipes.title} />
-                    <CardImage
-                        className="HomeRecipes__image--mt"
-                        image={recipes.img}></CardImage>
-                    <LikeButton></LikeButton>
-                </article></>
+        {recipes.map((recipes) => (
+            <Card
+              key={recipes.id}
+              image={recipes.img}
+              title={recipes.title}
+              firstInfo={recipes.firstInfo}
+              secondInfo={recipes.secondInfo}
+            ></Card>)
                 
             )
-        })}
+        }
     </div>
 )
 }
