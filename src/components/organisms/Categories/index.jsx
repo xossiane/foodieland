661d513fcf4c4c886651dc0d467a1bbffc98621c
KenjiWorkES <lucas.kenjies@gmail.com>
2022-06-14 14:20:47 -0300

    começando a colocar grid

--- src/components/organisms/Categories/index.jsx
+++ src/components/organisms/Categories/index.jsx
@@ -8,6 +8,11 @@ const Categories = () => {
     <section className="categories">
       <h2 className="categories__title">Categories</h2>
       <CategoriesCard image={Breakfast} text="Breakfast"></CategoriesCard>
+      <CategoriesCard image={Breakfast} text="Breakfast"></CategoriesCard>
+      <CategoriesCard image={Breakfast} text="Breakfast"></CategoriesCard>
+      <CategoriesCard image={Breakfast} text="Breakfast"></CategoriesCard>
+      <CategoriesCard image={Breakfast} text="Breakfast"></CategoriesCard>
+      <CategoriesCard image={Breakfast} text="Breakfast"></CategoriesCard>
       <Button>View All Categories</Button>
     </section>
   );
