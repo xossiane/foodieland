bebfcad198754b7fbfb4ea09b2ef962343f70af6
xossiane <anejosi.lima@gmail.com>
2022-06-16 15:41:00 -0300

    Melhoras no CSS do desktop e mobile, botões responsivos mas não com a função desejada (scrollby)

--- src/components/molecules/HomeRecipes/index.jsx
+++ src/components/molecules/HomeRecipes/index.jsx
@@ -6,34 +6,43 @@ import { Advertising } from "../../atoms";
 import leftarrow from "/assets/leftarrow.png"
 import rightarrow from "/assets/rightarrow.png"
 
+
+
 function HomeRecipes (props) {
     const recipes = homerecipes;
     const [isScrolling, setIsScrolling] = useState(false);
+   
+    
     const slider = useRef(null);
+    
 
-    const leftArrowClickHandler = () => {
-        slider.current.scrollBy(-290, 0);
-        disableBtnHandler();
-      };
+     const leftArrowClickHandler = () => {
+         window.scrollBy(-290, 0);
+         disabled={isScrolling}
+       };
     
-      const rightArrowClickHandler = () => {
-        slider.current.scrollBy(290, 0);
-        disableBtnHandler();
-      };
+       const rightArrowClickHandler = () => {
+         window.scrollBy(290, 0);
+         disabled={isScrolling}
+       };
+
+   
     
-      const disableBtnHandler = () => {
-        setIsScrolling(true);
-        setInterval(() => {
-          setIsScrolling(false);
-        }, 1000);
-      };
+       const disableBtnHandler = () => {
+         setIsScrolling(true);
+         setInterval(() => {
+           setIsScrolling(false);
+         }, 1000);
+       };
 
     
 return (
     <><header className="HomeRecipes__info">
         <h2 className="HomeRecipes__title">Simple and tasty recipes</h2>
         <p className="HomeRecipes__text">Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqut enim ad minim </p>
-    </header><div className="HomeRecipes__post">
+    </header>
+    
+    <div className="HomeRecipes__post">
             {recipes.map((recipes) => (
                 <div className="HomeRecipes__content" ref={slider}>
                     {<Card
@@ -55,9 +64,11 @@ return (
 
 
         </div>
+        
         <div className="HomeRecipes__slider__arrows">
-            <button className="leftArrow" onClick={leftArrowClickHandler} disabled={isScrolling} ><img src={leftarrow}></img></button> 
-            <button className="rightArrow" onClick={rightArrowClickHandler} disabled={isScrolling} ><img src={rightarrow}></img></button>
+                      
+            <button className="leftArrow" onClick={leftArrowClickHandler}  ><img src={leftarrow}></img></button> 
+            <button className="rightArrow" onClick={rightArrowClickHandler}  ><img src={rightarrow}></img></button>
             </div>
             </>
 )
