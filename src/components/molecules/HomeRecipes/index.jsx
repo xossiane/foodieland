c6e9604a9adf924e6f6e0c34b6b0e7d068a99d9d
KenjiWorkES <lucas.kenjies@gmail.com>
2022-06-17 13:09:25 -0300

    ajustando css do homeRecipes

--- src/components/molecules/HomeRecipes/index.jsx
+++ src/components/molecules/HomeRecipes/index.jsx
@@ -24,14 +24,13 @@ function HomeRecipes(props) {
 
   return (
     <>
-      <header className="HomeRecipes__info">
+      <div className="HomeRecipes__info">
         <h2 className="HomeRecipes__title">Simple and tasty recipes</h2>
         <p className="HomeRecipes__text">
           Lorem ipsum dolor sit amet, consectetuipisicing elit, sed do eiusmod
           tempor incididunt ut labore et dolore magna aliqut enim ad minim{" "}
         </p>
-      </header>
-
+      </div>
       <div className="HomeRecipes__post" ref={slider}>
         {recipes.map((recipes) => (
           <div className="HomeRecipes__content" key={recipes.id}>
