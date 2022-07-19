85085a27b12aff3a4dd390f394f924dc31fbcd43
xossiane <anejosi.lima@gmail.com>
2022-07-19 13:50:00 -0300

    Componentes aplicados pros valores nutricionais (via props) e CSS aplicado pro nutritional

--- src/components/organisms/RecipesBanner/index.jsx
+++ src/components/organisms/RecipesBanner/index.jsx
@@ -1,19 +1,32 @@
 import eggplate from "/assets/eggplate.png"
+import horizontalbar from "/assets/horizontalbar.png"
 import './RecipesBanner.scss'
+import { ArticleText } from "../../atoms";
 
 
 
 
 
 function RecipesBanner(){
-    const nutrition = [{name: "Calories", value: "219.9kcal"}, {name: "Total Fat", value:"10.7 g"}, {name: "Protein", value:"7.9g"}, {name: "Carbohydrate", value:"22.3g"}, {name: "Cholesterol",value:"37.4mg"}];
+    const nutrition = [
+        {type: "Calories", value: "219.9kcal"},
+         {type: "Total Fat", value:"10.7 g"}, 
+         {type: "Protein", value:"7.9g"}, 
+         {type: "Carbohydrate", value:"22.3g"}, 
+         {type: "Cholesterol",value:"37.4mg"}];
 
     return (
         <div className="RecipesBanner__container">
         <img className="RecipesBanner__img" src={eggplate} alt="" />
         <div className="RecipesBanner__nutritional">
             <h2 className="RecipesBanner__nutritional--title">Nutrition Information</h2>
-            <p>this is our content</p>
+            {nutrition.map(({type, value}) => (
+                <>
+                <h2 className="RecipesBanner__nutritional--type">{type}</h2>
+                <h2 className="RecipesBanner__nutritional--value">{value}</h2>
+                <img className="RecipesBanner__nutritionalBar" src={horizontalbar} alt="" />
+                </>))}
+                <p className="RecipesBanner__nutritional--text"> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
         </div>
         </div>
     )
