3ce017fd07d9d82b1eed0fe4fabc903dded879e1
xossiane <anejosi.lima@gmail.com>
2022-07-19 10:41:07 -0300

    Inserçao dos botões de share/print mas sem css aplicado. Finalização da barrinha de info

--- src/components/molecules/RecipesHeader/index.jsx
+++ src/components/molecules/RecipesHeader/index.jsx
@@ -2,10 +2,15 @@ import { ArticleAuthor, ArticleAvatar, ArticleDate} from '../../atoms';
 import bar from "/assets/bar.png"
 import Timer from "/assets/Timer.svg"
 import wade from "/assets/wade.png"
+import ForkKnife from "/assets/ForkKnife.png"
+import Print from "/assets/Print.png"
+import Share from "/assets/Share.png"
 import './RecipesHeader.scss'
 function RecipesHeader(){
     return (
-        <><p className="RecipesHeader__title">Health Japanese Fried Rice</p>
+        <>
+        <div className="RecipesHeader__container">
+        <p className="RecipesHeader__title">Health Japanese Fried Rice</p>
         <section className="RecipesHeader__info">
         <ArticleAvatar className="RecipesHeader--author" image={wade}/>
         <div className="RecipesHeader__info--text">
@@ -14,19 +19,31 @@ function RecipesHeader(){
         </div>
         <img className="RecipesHeader__info__bar" src={bar} alt="" />
         <img className="RecipesHeader__timer" src={Timer} />
+        {/* PrepTime (segunda tag) */}
         <div className="RecipesHeader__info--prepTime">
         <h2 className="RecipesHeader__info--prepTime1">Prep Time</h2>
         <h2 className="RecipesHeader__info--prepTime2">15 minutes</h2>
         </div>
-        <div className="RecipesHeader__info--cookTime">
+        
         <img className="RecipesHeader__info__bar2" src={bar} alt="" />
         <img className="RecipesHeader__timer--mt" src={Timer} />
+        {/* cookTime (terceira tag) */}
+        <div className="RecipesHeader__info--cookTime">
         <h2 className="RecipesHeader__info--cookTime1">Cook Time</h2>
         <h2 className="RecipesHeader__info--prepTime2">15 minutes</h2>
         </div>
+        <img className="RecipesHeader__info__bar2" src={bar} alt="" />
+        {/* type (quarta tag) */}
+        <img className="RecipesHeader__info--typeImg" src={ForkKnife} alt="" />
+        <h2 className="RecipesHeader__info--type">Chicken</h2>
+        
         </section>
         
-        
+        </div>
+        <div className="RecipesHeader__btn">
+        <img className="RecipesHeader__print" src={Print} alt="" />
+        <img className="RecipesHeader__share" src={Share} alt="" />
+        </div>
         
         </>
     )
